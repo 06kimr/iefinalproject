c28abe6eeb086c87bb73a07cb24e7c4d62d46029
06kimr <06kimr@gmail.com>
2021-12-06 13:40:40 +0900

    awd

--- DeepLearningModel/model.py
+++ DeepLearningModel/model.py
@@ -6,7 +6,7 @@ import numpy as np
 import torch
 from torchvision.transforms import transforms
 from models import densenet121, resmasking_dropout1
-import version
+from version import __version__
 
 
 def show(img, name="disp", width=1000):
